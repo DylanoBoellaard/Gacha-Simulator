03292411bcf8bae67ccd7dd1bfbe84a316c4166f
DylanoBoellaard <336751@student.mboutrecht.nl>
2026-01-30 22:10:53 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gacha Simulator</title>
+
+    @vite(['resources/scss/welcome.scss', 'resources/css/welcome.css' , 'resources/js/welcome.js'])
+</head>
+<body>
+    <h1>Welcome to the Gacha Simulator</h1>
+</body>
+</html>
